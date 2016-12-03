483689c5fdf3c6cac14264ffc1a0f589e89cc093
Tamas Dober <tamas.dober@mobileforming.com>
2016-12-03 00:46:02 -0800

    Upating app

--- module1-solution/assignment1-starter-code/app.js
+++ module1-solution/assignment1-starter-code/app.js
@@ -1,14 +1,39 @@
-(function functionName() {
+(function () {
 
   'use strict';
 
-angular.module('LunchCheckerApp', [])
-.controller('LunchCheckerController', function ($scope) {
-  $scope.listOfDishes =['soup','fish','salat'];
-  $scope.name="Tamas";
-  console.log(name);
-});
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
+  LunchCheckController.$inject = ['$scope'];
 
+  function LunchCheckController ($scope) {
+    $scope.inputList = "";
+    $scope.splittedList;
+    $scope.messageToDisplay = "";
+
+  $scope.splittedList = function  (){ 
+  	if (!$scope.inputList || !$scope.inputList.length) $scope.messageToDisplay = "Please enter data first"; 
+	splitString($scope.inputList,',');
+	countSplittedList();
+  }
+
+    function splitString(inputList,separator){
+  	//console.log(inputList);
+  	var splittedList = inputList.split(separator);
+  	//console.log(splittedList);
+  	$scope.splittedList = splittedList;
+    }
+
+    function countSplittedList(){
+    	var count = $scope.splittedList.length;
+    	console.log(count);
+    	if ($scope.splittedList[0].length == 0) return;
+    	if (count < 4 && count > 0) $scope.messageToDisplay = "Enjoy!";
+    	if (count >= 4) $scope.messageToDisplay = "Too much!";
+    }
+
+  }
 
 })();
+
