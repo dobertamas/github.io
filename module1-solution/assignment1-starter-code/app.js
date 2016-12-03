683dd41f964c62f7dcaf9a027383561e0b81d8d5
Tamas Dober <tamas.dober@mobileforming.com>
2016-12-03 00:42:26 -0800

    Initial commit

---
+++ module1-solution/assignment1-starter-code/app.js
@@ -0,0 +1,14 @@
+(function functionName() {
+
+  'use strict';
+
+angular.module('LunchCheckerApp', [])
+.controller('LunchCheckerController', function ($scope) {
+  $scope.listOfDishes =['soup','fish','salat'];
+  $scope.name="Tamas";
+  console.log(name);
+});
+
+
+
+})();
